de9388582d3c51b95a561d0c8ffcc40a3026dd0d
Stefan Atanasov <atanasovstefan@hotmail.com>
2024-03-04 00:34:51 +0200

    created theme colors object to store the colors throughtout the app

---
+++ src/utils/theme/colors.js
@@ -0,0 +1,11 @@
+const palette = {
+  primary: '#1B99F4',
+  secondary: '#35343D',
+  tertiary: '#425B71',
+  background: '#F1F2F4',
+  typography: '#35343D'
+}
+
+export const colors = {
+  palette
+}
